b3e69e9394147313b74735bfb3ebf8c340d26698
Sillas Teixeira Gonzaga <sillas.gonzaga@gmail.com>
2018-03-20 21:50:02 -0300

    upar arquivos da turm22a

--- R/m02_intro_igraph.R
+++ R/m02_intro_igraph.R
@@ -2,3 +2,6 @@
 library(tidyverse)
 library(igraph)
 
+#### Importacao dos dados
+df_nos <- read_csv("https://raw.githubusercontent.com/sillasgonzaga/curso_redes_sociais/master/data/turma_faculdade_nos.csv")
+df_vertices <- read_csv("https://raw.githubusercontent.com/sillasgonzaga/curso_redes_sociais/master/data/turma_faculdade_vertices.csv")
